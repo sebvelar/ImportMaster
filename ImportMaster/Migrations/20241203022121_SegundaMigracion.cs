331670ec6cd12ae171ab1587be109daaa4e6f849
sebvelar <sebvelar@gmail.com>
2024-12-02 20:33:50 -0600

    Agregar archivos de proyecto.

---
+++ ImportMaster/Migrations/20241203022121_SegundaMigracion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImportMaster.Migrations
+{
+    /// <inheritdoc />
+    public partial class SegundaMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ ImportMaster/Models/Producto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagement.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+
+        [StringLength(255)]
+        public string Descripcion { get; set; }
+
+
+        public int Existencias { get; set; }
+
+        public float Precio { get; set; }
+
+        public int PorcentajeImpuesto { get; set; }
+
+        public int OrdenCompraId { get; set; }
+
+        public ICollection<OrdenCompraProducto> OrdenCompraProductos { get; set; }
+
+
+
+
+    }
+}
